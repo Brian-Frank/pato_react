84548f508944bd7c5774208168fe072b7002ec01
Brian Frank <86387416+Brian-Frank@users.noreply.github.com>
2022-03-03 00:38:02 -0300

    Desafio contador

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import './App.css';
 import Navv from './components/Navv';
 import ItemListContainer from './components/ItemListContainer';
 
-
 function App() {
   return (
     <div className="App">
       <Navv/>
       <ItemListContainer nombre="Tia Pato"/>
-      <h1>React-TiaPato</h1>
     </div>
   );
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import ItemCount from './ItemCount'
 
 const ItemListContainer = (props) => {
 
     console.log(props.nombre)
+
   return (
     <div className='container'>
-
+      <h1>Productos</h1>
+      <ItemCount stock={5}  initial={1}/>
     </div>
   )
 }
